a63b009edca60648e22c5106fddbeee043df9d6e
Aalia98 <aaliaamin98@gmail.com>
2023-08-11 19:58:59 +0530

    initial commit

---
+++ src/redux/store.js
@@ -0,0 +1,13 @@
+import { configureStore } from "@reduxjs/toolkit";
+import boardsSlice from './boardsSlice'
+
+
+const store = configureStore({
+    reducer: {
+        boards: boardsSlice.reducer,
+    }
+})
+
+
+
+export default store
